bc34d487b8fd07b15e00077d006958fe066fa2e6
E-Kuru <evandalleaublop@gmail.com>
2021-10-25 14:41:49 +0200

    Exo Done

---
+++ src/components/Box.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import '../App.css';
+
+
+
+class Box extends React.Component {
+  constructor(){
+    super()
+  }
+  render() {
+    return (
+        <div className='box col-sm-3 col-6'>
+            <span class="material-icons" style={{
+              fontSize : 100,
+              color : `${this.props.color}`
+            }}>
+                {this.props.icon}
+            </span>
+            <p>{this.props.value} {this.props.unit}</p>
+
+        </div>
+    );
+  }
+}
+
+export default Box;
